9ec8816e9f2c3c8abdbb1535bec57a45ae8853b3
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-06-06 21:34:39 -0400

    commit tree 0a5aeb863a5c45f8199456afe87c3fe667c1ada7

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const body = document.querySelector("body");
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Tony is the champion";
+body.append(newHeader);
